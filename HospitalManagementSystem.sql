815076c07c963d1b4df564bffbb06416b3069709
Ali Roohan <aliroohan321@gmail.com>
2025-06-16 13:01:54 +0500

    removed extra functions

--- HospitalManagementSystem.sql
+++ HospitalManagementSystem.sql
@@ -9,7 +9,7 @@ CREATE TABLE Users (
     user_id INT IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL CHECK (role IN ('Admin', 'Appointment', 'Patient'))
+    role VARCHAR(50) NOT NULL CHECK (role IN ('Admin', 'Appointment', 'Patient', 'Billing'))
 );
 GO
 
